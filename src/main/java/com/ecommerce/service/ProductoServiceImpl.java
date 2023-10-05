4e4f8227e6e657c1096fa2fb082620598e824a02
Prueba <jorgel123456@users.noreply.github.com>
2023-10-05 16:51:54 -0500

    funcionalidad de mostrar proctos para el admioistrador

--- src/main/java/com/ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/ecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,5 +36,12 @@ public class ProductoServiceImpl implements ProductoService{
 		productoRepository.deleteById(id);
 		
 	}
+
+	@Override
+	public List<Producto> findAll() {
+		// TODO Auto-generated method stub
+		return productoRepository.findAll();
+	}
+	
 
 }
