8a78cfdc1cf616c33cfe9c30ee4a8f15419cf73c
Prueba <jorgel123456@users.noreply.github.com>
2023-10-07 21:04:41 -0500

    se creo la class de servicio para guardar y eliminar una imagende producto

---
+++ src/main/java/com/ecommerce/service/UploadfileService.java
@@ -0,0 +1,33 @@
+package com.ecommerce.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadfileService {
+	private String folder="images";
+	
+	public String saveIage(MultipartFile file) throws IOException {
+		if(!file.isEmpty()) {
+			byte[] bytes=file.getBytes();
+			Path path = Paths.get(folder + file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg";
+	}
+	
+	public void deleteImage(String nombre) {
+		String ruta="images//";
+		File file=new File(ruta+nombre);
+		file.delete();
+	}
+
+}
